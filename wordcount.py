2ef3b7728ac21c95af7afddf5d78f0328ccc2f5c
Sandrita <sandramilenagarciaa@gmail.com>
2022-11-14 14:15:51 -0800

    create dictionary

--- wordcount.py
+++ wordcount.py
@@ -2,3 +2,23 @@
 
 
 # put your code here.
+
+
+def space_count(filename):
+
+    import_file = open(filename)
+    word_counter = {}
+
+    for line in import_file:
+
+        words = line.split(' ')
+
+        for word in words:
+
+            word_counter[word] = word_counter.get(word, 0) + 1
+
+    return word_counter
+
+
+var = space_count('test.txt')
+print(var)
